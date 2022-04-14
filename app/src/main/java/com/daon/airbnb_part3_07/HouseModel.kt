110f9633d44757797def9673823dd5dfbd229909
daon1213 <diaeiyo1213@naver.com>
2022-04-15 08:10:08 +0900

    House Model commit!

---
+++ app/src/main/java/com/daon/airbnb_part3_07/HouseModel.kt
@@ -0,0 +1,10 @@
+package com.daon.airbnb_part3_07
+
+data class HouseModel(
+    val id: Int,
+    val title: String,
+    val price: String,
+    val imgUrl: String,
+    val lat: Double,
+    val lng: Double
+)
